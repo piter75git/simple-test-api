f6e38d1a0e91a167ca05cfea80fd75bd7a3b8c5e
Piotr Murza <piotr.murza@gmail.com>
2020-08-23 12:07:00 +0100

    Add routes. Switch off unused route types.

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('channels', 'ChannelController@index');
+Route::get('channels/{channel_uuid}/programmes/{programme_uuid}', 'Channel\ProgrammeController@show');
+Route::get('channels/{channel_uuid}/{date}/{timezone}', 'Channel\TimetableController@index');
+
+Route::get('/', 'InfoController@index');
