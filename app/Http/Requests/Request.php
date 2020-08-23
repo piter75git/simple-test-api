db540d2f92af336784a171774ee86b83d523af1a
Piotr Murza <piotr.murza@gmail.com>
2020-08-23 12:03:38 +0100

    Create Request and Response

---
+++ app/Http/Requests/Request.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace App\Http\Requests;
+
+use Illuminate\Http\Request as HttpRequest;
+
+class Request extends HttpRequest
+{
+    //
+}
---
+++ app/Http/Responses/Response.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace App\Http\Responses;
+
+use Illuminate\Http\Response as HttpResponse;
+
+class Response extends HttpResponse
+{
+    //
+}
