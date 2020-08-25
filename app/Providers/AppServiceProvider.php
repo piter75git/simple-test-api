d4370f69b25a5adc975ca776e2a8060759ac5829
Piotr Murza <piotr.murza@gmail.com>
2020-08-25 16:49:42 +0100

    Refactor the app service provider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use App\Models\Timezone;
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
 
@@ -23,6 +24,17 @@ class AppServiceProvider extends ServiceProvider
      * @return void
      */
     public function boot()
+    {
+        $this->applyMySqlLimitation();
+    }
+
+    /**
+     * Apply the MySql limitation for strings
+     * to support older MySql versions.
+     *
+     * @return void
+     */
+    private function applyMySqlLimitation(): void
     {
         Schema::defaultStringLength(191);
     }
